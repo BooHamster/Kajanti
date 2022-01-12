88cca66afd5373e06173fefd3ad2eb95dfdb7f6f
BooHamster <robert.kajanti@gmail.com>
2022-01-12 11:14:34 +0200

    Koira Harjoitus

---
+++ Harkat/OlioJaWPFSovellukset/Harjoitus1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Harjoitus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Koira mopsi = new Koira("Höpö", 15, "Mopsi"); //Luo Uuden Olion
+            Koira Mäyrä = new Koira("Töpö", 10, "MäyräKoira");
+            Koira Kultainen = new Koira("Löpö", 5, "KultainenNoutaja");
+            mopsi.Nimi = "Höpö";
+            mopsi.Ikä = 15;
+            mopsi.Rotu = "Mopsi";
+
+            Mäyrä.Nimi = "Töpö";
+            Mäyrä.Ikä = 10;
+            Mäyrä.Rotu = "Mäyräkoira";
+
+
+            Console.WriteLine(mopsi.HaeTiedot());
+            mopsi.Hauku();
+            mopsi.Hauku();
+            mopsi.Hauku();
+
+            Console.WriteLine(Mäyrä.HaeTiedot());
+            Mäyrä.Hauku();
+            Mäyrä.Hauku();
+            Mäyrä.Hauku();
+            */
+
+        }
+
+    }
+}
