f2ccfa3d0972e28e564150f06a4c0b2dbbbfbfa6
BooHamster <robert.kajanti@gmail.com>
2022-01-12 21:12:34 +0200

    Harjoitus1 Commit

--- Harkat/OlioJaWPFSovellukset/Harjoitus1/Program.cs
+++ Harkat/OlioJaWPFSovellukset/Harjoitus1/Program.cs
@@ -6,6 +6,35 @@ namespace Harjoitus1
     {
         static void Main(string[] args)
         {
+            Ajoneuvo auto = new Ajoneuvo(); // Olio Ajoneuvo luokasta
+            //Asetetaan oliolle "auto" arvot
+            auto.Nimi = "Toyta";
+            auto.Nopeus = 160;
+            auto.Renkaat = 4;
+
+            // Käytetään Ajoneuvo luokan toimintoja
+            auto.TulostaData();
+
+            //Noudetaan auton tiedot ToString() metodilla
+            string autontiedot = auto.ToString();
+            Console.WriteLine(autontiedot);
+
+            //Tyhjä rivi konsoliibn
+            Console.WriteLine();
+
+            //Luodaan toinen olio luokasta Ajoneuvo
+
+            Ajoneuvo mopo = new Ajoneuvo(); // Olio ajoneuvo luokasta
+            mopo.Nimi = "Yamaha";
+            mopo.Nopeus = 200;
+            mopo.Renkaat = 2;
+
+            // Käytetään Ajoneuvo luokan toimintoja
+            mopo.TulostaData();
+
+           
+            Console.WriteLine(mopo.ToString());
+
             /*
             Koira mopsi = new Koira("Höpö", 15, "Mopsi"); //Luo Uuden Olion
             Koira Mäyrä = new Koira("Töpö", 10, "MäyräKoira");
