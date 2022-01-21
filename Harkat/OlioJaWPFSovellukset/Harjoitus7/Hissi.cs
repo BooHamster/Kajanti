f75e05f19c7fe331dc4d8e20c53eeb13ecc01d7c
BooHamster <robert.kajanti@gmail.com>
2022-01-21 12:04:52 +0200

    Harjoitus7

---
+++ Harkat/OlioJaWPFSovellukset/Harjoitus7/Hissi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus7
+{
+    class Hissi
+    {
+        private readonly int MaxKerros = 6;
+        private int kerros { get; set; }
+        public int Kerros
+        {
+            get { return kerros; }
+            set 
+            {
+                if (value> MaxKerros)
+                {
+                    value = MaxKerros;
+                }
+                kerros = value;
+            }
+        }
+        public void AsetaKerros()
+        {
+            Kerros= Convert.ToInt32(Console.ReadLine()); //Tämä vaihtaa annetun arvon Console.Readline() komennosta Int muotoon jotta loput koodista pystytään suorittamaan
+            Console.WriteLine("Olet kerroksessa: " + Kerros.ToString());          
+        }
+
+    }
+}
---
+++ Harkat/OlioJaWPFSovellukset/Harjoitus7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Harjoitus7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean jatka = true;
+            Hissi Hissi1 = new Hissi();
+
+            while (jatka != false)
+            {
+                try
+                {
+                    Console.Write("Olet hississä jossa on 1-6 kerrosta, mihin kerrokseen haluaisit mennä? Anna kerroksen numero: ");
+                    Hissi1.AsetaKerros();
+                    Console.WriteLine("jos haluat poistua hissistä paina 'p' nappia: ");
+                    if (Console.ReadKey(true).Key == ConsoleKey.P)
+                    {
+                        Console.WriteLine("Poistuit hissistä");
+                        jatka = false;
+                    }
+                    else
+                    {
+                        Hissi1.AsetaKerros();
+                    }
+                   
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Annoit väärän toiminnon");
+                }
+            }
+            //Hissi1.Kerros = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Olet kerroksessa: " + Hissi1.Kerros.ToString());
+
+        }
+    }
+}
